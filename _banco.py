386ccf1474efbceb1ff24bc178e0975fd943cabd
Leonardo Santos <leonardodss2713@yahoo.com>
2023-05-02 13:00:55 -0300

    _

---
+++ _banco.py
@@ -0,0 +1,45 @@
+import sqlite3
+import os
+from datetime import date
+os.system('cls' if os.name == 'nt' else 'clear')#limpar terminarl
+
+
+lojas=['eshop','nuuvem','psn','steam']
+rowlist_banco=[]
+def pesquisa_banco():   
+    pesquisa_line=input("Digite o nome do jogo: ")
+    print('Filtrar Data ')
+    data_pesquisa=input("Digite a data da pesquisa(XX/XX/XXXX): ")
+    filtro="jogo"
+    order_by='ASC'
+    for loja_ in lojas:
+            try:
+                conexao=sqlite3.connect("jogos.db")
+                cursor=conexao.cursor()
+                if filtro=='jogo':
+                    cursor.execute(f"SELECT * FROM {loja_} WHERE jogo LIKE '%{pesquisa_line}%' order by  {filtro} {order_by};")
+                if filtro=='preco':
+                    cursor.execute(f"SELECT * FROM {loja_} ORDER BY LENGTH ({filtro}) {order_by};")
+                resultado=cursor.fetchone()
+                while True:
+                    resultado=cursor.fetchone()
+                    if resultado is None:
+                        break
+                    
+                    loja,jogo,preco,pdesconto,poriginal,linkcompleto,tipo,data=resultado
+                    
+                    rowlist_banco.append((loja,jogo,preco,pdesconto,poriginal,linkcompleto,tipo,data))
+                cursor.close()
+                conexao.close()
+            except:
+                    print('listra ainda nao criada')
+    print_(data_pesquisa)
+def print_(data_pesquisa):
+    for r in rowlist_banco:
+        loja,jogo,preco,pdesconto,poriginal,linkcompleto,tipo,data=r
+        if data_pesquisa=='':
+            print(loja.upper(),jogo,preco,pdesconto,poriginal,linkcompleto,tipo,data)   
+        if data_pesquisa==data:
+            print(loja.upper(),jogo,preco,pdesconto,poriginal,linkcompleto,tipo,data)
+    print('')
+pesquisa_banco()
